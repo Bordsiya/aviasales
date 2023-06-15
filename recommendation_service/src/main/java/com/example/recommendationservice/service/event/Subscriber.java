a2a796fde464d90878ac6a0208de55613b1e08fc
flyfalldie <kolya_cypandin@mail.ru>
2023-06-15 20:33:08 +0300

    Recommendation Service listener

---
+++ recommendation_service/src/main/java/com/example/recommendationservice/service/event/Subscriber.java
@@ -0,0 +1,23 @@
+package com.example.recommendationservice.service.event;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "mailRequestQueue")
+@RequiredArgsConstructor
+public class Subscriber {
+    private final ObjectMapper mapper;
+    private final BuyTicketEventWorker worker;
+
+    @RabbitHandler
+    public void receivedMessage(String msg) throws JsonProcessingException {
+        System.out.println("Got buy ticket event: " + msg);
+        BuyTicketEvent event = mapper.readValue(msg, BuyTicketEvent.class);
+        worker.process(event);
+    }
+}
