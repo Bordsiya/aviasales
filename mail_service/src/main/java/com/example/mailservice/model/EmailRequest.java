f0c8377aaabda1e79af622708db8c810159a7328
flyfalldie <kolya_cypandin@mail.ru>
2023-06-17 16:20:36 +0300

    Logs added

--- mail_service/src/main/java/com/example/mailservice/model/EmailRequest.java
+++ mail_service/src/main/java/com/example/mailservice/model/EmailRequest.java
@@ -4,14 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class EmailRequest {
-        private Long mailRequestId;
-        private String email;
-        private String subject;
-        private String text;
+    private Long mailRequestId;
+    private String email;
+    private String subject;
+    private String text;
 }
